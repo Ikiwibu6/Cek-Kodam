7a5e4753e53d6c9dfa7cc2421a9b2618446b660a
Ikiwibu6 <147376009+Ikiwibu6@users.noreply.github.com>
2024-12-07 09:17:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let input = document.getElementById("input");
+let tombol = document.getElementById("tombol");
+let hasil = document.getElementById("hasil");
+let nama =document.getElementById("nama")
+
+tombol.addEventListener("click", cekKhodam);
+
+function cekKhodam() {
+    nama.innerHTML  = input.value;
+    let angkaRendam = Math.floor(Math.random() * namaKhodam.length);
+    hasil.innerHTML = `${namaKhodam[angkaRendam]}`;
+};
+
+const namaKhodam = [
+    "Ular",
+    "Monyet",
+    "Naga",
+    "Kucing Emas",
+    "Kecantikan",
+    "Macan",
+    "Harimau",
+    "Elang",
+    "Kuda",
+    "Singa",
+    "Burung Hantu",
+    "Buaya",
+    "Serigala",
+    "Ayam Jantan",
+    "Botol Yakult",
+    "Tutup Botol",
+    "Tikus Got",
+    "Chetag Olab",
+    "Dewa",
+    "Tukang Es Cincau",
+    "Dajjal",
+    "Samsung",
+    "Semut",
+    "Kutu Rambut",
+    "Supra Bapak",
+    "Sapu",
+    "Sapi"
+];
